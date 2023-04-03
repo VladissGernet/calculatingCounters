30fd67b07ce8960a6b2825a3d104d447dd9af6f9
Vlad <skeimorr@gmail.com>
2023-04-03 11:14:06 +0400

    the code is shortered

--- source/js/calc.js
+++ source/js/calc.js
@@ -7,18 +7,17 @@ document.getElementById("water").textContent = waterConst.toString();
 
 let elecInputBefore = document.getElementById("calc-elec-before");
 let elecInputAfter = document.getElementById("calc-elec-after");
-let elecResult = document.getElementById("calc-elec-result");
-
 let gasInputBefore = document.getElementById("calc-gas-before");
 let gasInputAfter = document.getElementById("calc-gas-after");
-let gasResult = document.getElementById("calc-gas-result");
-
 let waterInputBefore = document.getElementById("calc-water-before");
 let waterInputAfter = document.getElementById("calc-water-after");
+
+let elecResult = document.getElementById("calc-elec-result");
+let gasResult = document.getElementById("calc-gas-result");
 let waterResult = document.getElementById("calc-water-result");
+let resultTotal = document.querySelector(".calc-form__total-result");
 
 let buttonTotal = document.querySelector(".calc-form__button");
-let resultTotal = document.querySelector(".calc-form__total-result");
 
 buttonTotal.onclick = function () {
   let elecCalc = (elecInputAfter.value - elecInputBefore.value) * elecConst;
@@ -29,30 +28,10 @@ buttonTotal.onclick = function () {
   elecResult.textContent = elecCalc.toString() + " руб.";
   gasResult.textContent = gasCalc.toString() + " руб.";
   waterResult.textContent = waterCalc.toString() + " руб.";
-
   resultTotal.textContent = calcTotal.toString() + " руб.";
 
-  if (elecCalc < 0) {
-    elecResult.style.color = "red";
-  } else {
-    elecResult.style.color = "black";
-  }
-
-  if (gasCalc < 0) {
-    gasResult.style.color = "red";
-  } else {
-    gasResult.style.color = "black";
-  }
-
-  if (waterCalc < 0) {
-    waterResult.style.color = "red";
-  } else {
-    waterResult.style.color = "black";
-  }
-
-  if (calcTotal < 0) {
-    resultTotal.style.color = "red";
-  } else {
-    resultTotal.style.color = "black";
-  }
+  elecResult.style.color = (elecCalc < 0 ? "red" : "black");
+  gasResult.style.color = (gasCalc < 0 ? "red" : "black");
+  waterResult.style.color = (waterCalc < 0 ? "red" : "black");
+  resultTotal.style.color = (calcTotal < 0 ? "red" : "black");
 }
